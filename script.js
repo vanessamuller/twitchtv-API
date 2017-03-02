58ec4cc87eee9d0c28e88f4792c11f400d1f3183
vanessamuller <vane9090@gmail.com>
2017-03-01 19:08:11 -0500

    added Javascript to show information about user who are currently streaming

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 $(document).ready(function(){
+
   $.getJSON("https://wind-bow.gomix.me/twitch-api/streams/freecodecamp",function(data){
     if(data.stream===null){
       $(".status").html("Free Code Camp is OFFLINE")
@@ -6,14 +7,24 @@ $(document).ready(function(){
     else{
       $(".status").html("Free Code Camp is ONLINE")
     }
-  })
+  });
+
+  var users = ["ESL_SC2", "OgamingSC2", "cretetion", "freecodecamp", "storbeck", "habathcx", "RobotCaleb", "noobs2ninjas", "comster404"];
+
+  for(var i=0; i<users.length; i++){
+    $.getJSON("https://wind-bow.gomix.me/twitch-api/streams/"+users[i],function(res){
+      if(res.stream===null){
+
+      }
+      else{
+      var logo =res.stream.channel.logo;
+      var name= res.stream.channel.display_name;
+      var status = res.stream.channel.status;
+        $("#followerInfo").prepend("<div class='row line'>"+ '<div class="col-md-4">'+ "<img class='img-responsive' src=" + logo+ ">"+ "</div>" + '<div class="col-md-4">'+ name + '</div>' +'<div class="col-md-4">' + status +'</div> </div>');
+      };
+      console.log(name);
 
 
-
-
-
-
-
-
-
-})
+    });
+  };
+});
