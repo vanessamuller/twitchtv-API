87438b3f08cce8bf6e8e02c763a2e10705a8a8cf
vanessamuller <vane9090@gmail.com>
2017-03-01 22:24:47 -0500

    fixed classes in jquery  html

--- script.js
+++ script.js
@@ -17,7 +17,7 @@ $(document).ready(function(){
         var logo =res.stream.channel.logo;
         var name= res.stream.channel.display_name;
         var status = res.stream.channel.status;
-        $("#followerInfo").prepend("<div class='row line'>"+ '<div class="col-md-4">'+ "<img class='img-responsive' src=" + logo+ ">"+ "</div>" + '<div class="col-md-4">'+ name + '</div>' +'<div class="col-md-4">' + status +'</div> </div>');
+        $("#followerInfo").prepend("<div class='row line'>"+ '<div class="col-md-4 text-center center-block">'+ "<img class='img-responsive' src=" + logo+ ">"+ "</div>" + '<div class="col-md-4 sta text-center">'+ name + '</div>' +'<div class="col-md-4 sta text-center">' + status +'</div> </div>');
       };
 
       if(res.stream===null){
@@ -25,17 +25,14 @@ $(document).ready(function(){
         $.getJSON("https://wind-bow.gomix.me/twitch-api/channels/"+name,function(rest){
           if(rest.error){
             var status3=rest.message;
-            $("#followerInfo").prepend("<div class='row line'>"+ '<div class="col-md-4">'+ "<img class='img-responsive' src='http://i0.kym-cdn.com/profiles/icons/big/000/071/040/404%20is%20not%20found.jpg' >"+ "</div>" + '<div class="col-md-4">'+ name + '</div>' +'<div class="col-md-4">' + status3 +'</div> </div>');
+            $("#followerInfo").prepend("<div class='row line'>"+ '<div class="col-md-4 sta ">'+ "<img class='img-responsive' src='http://i0.kym-cdn.com/profiles/icons/big/000/071/040/404%20is%20not%20found.jpg' >"+ "</div>" + '<div class="col-md-4 sta text-center">'+ name + '</div>' +'<div class="col-md-4 sta text-center">' + status3 +'</div> </div>');
           }
           else{
             var logo2= rest.logo;
-            $("#followerInfo").prepend("<div class='row line'>"+ '<div class="col-md-4">'+ "<img class='img-responsive' src=" + logo2+ ">"+ "</div>" + '<div class="col-md-4">'+ name + '</div>' +'<div class="col-md-4">OFFLINE</div> </div>');
+            $("#followerInfo").prepend("<div class='row line'>"+ '<div class="col-md-4 text-center">'+ "<img class='img-responsive' src=" + logo2+ ">"+ "</div>" + '<div class="col-md-4 sta text-center">'+ name + '</div>' +'<div class="col-md-4 sta text-center">OFFLINE</div> </div>');
           };
         });
       };
     });
-  };
-
-
-
+  };  
 });
